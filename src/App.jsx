795dc78a68e650d2ccab6a4f7de95b19d1d2e282
MBANS8A1 <seanmbng@aol.com>
2026-02-06 12:47:11 +0000

    In App.jsx, I included some styled components with CSS styling applied to them . I performed the aforementioned for the App component parent JSX div, h1 element, input elements and button elements. This want done to ensure the vscode-styled-components extension I installed was working as expected.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,39 @@ import styled from "styled-components";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
 `;
 function App() {
   return (
-    <div>
+    <StyledApp>
       <H1>The Wild Grainy Sanctum</H1>
-    </div>
+      <Button onClick={() => alert("Checked in")}>Check in</Button>
+      <Button onClick={() => alert("Checked out")}>Check out</Button>
+      <Input type="number" placeholder="Number of guests..." />
+    </StyledApp>
   );
 }
 
