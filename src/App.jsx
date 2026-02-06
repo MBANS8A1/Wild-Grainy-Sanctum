cbb14b8b69c6f954a59d08d0fac04b4eff75c785
MBANS8A1 <seanmbng@aol.com>
2026-02-06 23:20:34 +0000

    I moved the Input and Button styled components implementation to separate files in the ui folder and then exported them back into the App.jsx file. I also added a hover state to the Button styled component in Button.jsx, which delivers a brand-700 shade with a transition.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  cursor: pointer;
-  box-shadow: var(--shadow-sm);
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  box-shadow: var(--shadow-sm);
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
