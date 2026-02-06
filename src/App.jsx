ec301dc0c84a2361753212dbf0b79c3a71ac83a5
MBANS8A1 <seanmbng@aol.com>
2026-02-06 17:49:07 +0000

    I created a GlobalStyles.js file in the styles folder, which uses the CSS in the index.css globally via createGlobalStyle styled component. After it has been exported, I included it as a self-closing sibling component to the StyledApp component in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -30,12 +31,15 @@ const StyledApp = styled.div`
 `;
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Grainy Sanctum</H1>
-      <Button onClick={() => alert("Checked in")}>Check in</Button>
-      <Button onClick={() => alert("Checked out")}>Check out</Button>
-      <Input type="number" placeholder="Number of guests..." />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Grainy Sanctum</H1>
+        <Button onClick={() => alert("Checked in")}>Check in</Button>
+        <Button onClick={() => alert("Checked out")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests..." />
+      </StyledApp>
+    </>
   );
 }
 
