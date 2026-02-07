40da300ca6bded2f2ac937f0ddbb4332930c44e1
MBANS8A1 <seanmbng@aol.com>
2026-02-07 17:46:20 +0000

    I want to segment the Button components and form Input components into separate rows. So I made custom Row styled component in the ui folder (Row.jsx), which is a styled div for now. I have already applied it to App.jsx where each Row contains a heading and a nested div with either Buttons or Input component instances.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -13,12 +14,21 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Grainy Sanctum</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Checked in")}>Check in</Button>
-        <Button onClick={() => alert("Checked out")}>Check out</Button>
-        <Heading as="h3">Forms</Heading>
-        <Input type="number" placeholder="Number of guests..." />
+        <Row>
+          <Heading as="h1">The Wild Grainy Sanctum</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button onClick={() => alert("Checked in")}>Check in</Button>
+            <Button onClick={() => alert("Checked out")}>Check out</Button>
+          </div>
+        </Row>
+        <Row>
+          <Heading as="h3">Forms</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests..." />
+            <Input type="number" placeholder="Number of guests..." />
+          </form>
+        </Row>
       </StyledApp>
     </>
   );
