2ad281f367d3ba51a95d4f0bb129df258536741a
MBANS8A1 <seanmbng@aol.com>
2026-02-08 23:00:51 +0000

    I converted the <main> element to a styled Main component with CSS properties using template literals. I did the same for the <header> and <aside> elements in the Header.jsx and Sidebar.jsx files, respectively.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 function AppLayout() {
+  const Main = styled.main`
+    background-color: green;
+    padding: 4rem 4.8rem 6.4rem;
+  `;
+
   return (
     <div>
       <Header />
       <Sidebar />
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
     </div>
   );
 }
