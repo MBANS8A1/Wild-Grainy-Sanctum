bacdb24bd0c4c476779a32f23038136da49eb39e
MBANS8A1 <seanmbng@aol.com>
2026-02-08 20:14:58 +0000

    To start the design of the layout page, I created a Header and Sidebar component in separate files(Header.jsx and Sidebar.jsx) and then included them in the AppLayout component.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 function AppLayout() {
   return (
     <div>
-      <p>APP LAYOUT</p>
+      <Header />
+      <Sidebar />
       <Outlet />
     </div>
   );
