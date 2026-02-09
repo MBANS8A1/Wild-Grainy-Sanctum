0b55bed161e52acd588ffaf20cb357741c21de53
MBANS8A1 <seanmbng@aol.com>
2026-02-09 09:46:14 +0000

    In the main layout file, I turned the outer <div> into a grid container where the first column is a fixed 26 rem, whilst the remaining space is flexible available space.  The rows of the container autofit the content and the rest will take up the remaining space; the StyledAppLayout takes up the full height of the viewport. Since the Sidebar is a grid item, to make it move to the left, I used the grid-row property to make it span from the first row to the last row with the StyledSidebar styled component.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,6 +3,13 @@ import styled from "styled-components";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
 function AppLayout() {
   const Main = styled.main`
     background-color: green;
@@ -10,13 +17,13 @@ function AppLayout() {
   `;
 
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
         <Outlet />
       </Main>
-    </div>
+    </StyledAppLayout>
   );
 }
 
