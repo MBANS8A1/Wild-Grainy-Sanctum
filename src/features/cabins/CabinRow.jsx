1155c77d1103548ea8cb956b300801ac2ddb1689
MBANS8A1 <seanmbng@aol.com>
2026-02-13 18:53:02 +0000

    I destructured the isPending and mutate properties off of the useMutation hook, which uses the deleteCabin API function. I used the mutate function within the onClick prop of the "Delete" button element and isDeleting(from renaming isPending) within the disabled prop of the "Delete" button.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -43,9 +43,16 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
 
-  useMutation({
+  const { isPending: isDeleting, mutate } = useMutation({
     mutationFn: (id) => deleteCabin(id),
   });
 
@@ -56,7 +63,9 @@ function CabinRow({ cabin }) {
       <div>Fits a maximum of {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
   );
 }
