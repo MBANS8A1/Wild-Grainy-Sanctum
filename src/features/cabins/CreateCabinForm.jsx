1a007b0c3b44403ef5b10e82eec9ef63358d718d
MBANS8A1 <seanmbng@aol.com>
2026-02-17 18:28:50 +0000

    To pass the image uploaded to the FileInput component with the rest of the data, I passed in a new object into the mutate() function inside of the onFormSubmission handler function. This new object spreads the other Form Input data and passed the image property with the File at FileList position 0 in the array; position 1 has the length, which I do not need.

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -29,7 +29,8 @@ function CreateCabinForm() {
   });
 
   function onFormSubmission(data) {
-    mutate(data);
+    console.log(data);
+    mutate({ ...data, image: data.image.at(0) });
   }
 
   function onError(errors) {
