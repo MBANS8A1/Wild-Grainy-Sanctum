e16f78292ba628bfbb6f0f77d1f0ab1320d727cb
MBANS8A1 <seanmbng@aol.com>
2026-02-20 01:05:49 +0000

    I created a useDeleteCabin.js file inside the cabins feature folder because I want the deletion cabin mutation logic extracted out of the CabinRow component to create a reusable custom hook.

---
+++ src/features/cabins/useDeleteCabin.js
@@ -0,0 +1,14 @@
+export function useDeleteCabin() {
+  const queryClient = useQueryClient();
+
+  const { isPending: isDeleting, mutate } = useMutation({
+    mutationFn: (id) => deleteCabin(id),
+    onSuccess: () => {
+      toast.success("Cabin successfully deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+}
