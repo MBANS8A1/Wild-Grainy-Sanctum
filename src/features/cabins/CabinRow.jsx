9d05913971ba4b6caf6f47d803a461fca3a8e301
MBANS8A1 <seanmbng@aol.com>
2026-02-20 20:52:25 +0000

    In the CabinRow.jsx file, I decided to replace the text for the buttons associated with each cabin with react-icons, since I have that library available.  HiSquare2Stack for "Duplicate", HiPencil for "Edit" and HiTrash for "Delete". I did this because using text was making the cabin rows cramped.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
+import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 
 const TableRow = styled.div`
   display: grid;
@@ -69,9 +70,14 @@ function CabinRow({ cabin }) {
           <span>&mdash;</span>
         )}
         <div>
-          <button onClick={() => setShowForm((show) => !show)}>Edit</button>
+          <button>
+            <HiSquare2Stack />
+          </button>
+          <button onClick={() => setShowForm((show) => !show)}>
+            <HiPencil />
+          </button>
           <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-            Delete
+            <HiTrash />
           </button>
         </div>
       </TableRow>
