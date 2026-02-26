f61c63207cf3aa0f537eb131a6f9760ce3f3efea
MBANS8A1 <seanmbng@aol.com>
2026-02-26 09:24:14 +0000

    I instead passed the CreateCabinForm component instance within the Modal component as a child in AddCabin.jsx. I want the Modal component to be reusable, so it will receive the children prop.

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   const [isOpenModal, setIsOpenModal] = useState(false);
@@ -10,7 +11,11 @@ function AddCabin() {
       <Button onClick={() => setIsOpenModal((show) => !show)}>
         {!isOpenModal ? "Add new cabin" : "Hide create cabin form"}
       </Button>
-      {isOpenModal && <Modal />}
+      {isOpenModal && (
+        <Modal>
+          <CreateCabinForm />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -49,8 +49,8 @@ const Button = styled.button`
   }
 `;
 
-function Modal() {
-  return <StyledModal>Modal</StyledModal>;
+function Modal({ children }) {
+  return <StyledModal>{children}</StyledModal>;
 }
 
 export default Modal;
