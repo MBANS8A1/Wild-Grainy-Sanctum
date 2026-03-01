0a0f50a281ab66bde25b2fd81884870572eb1905
MBANS8A1 <seanmbng@aol.com>
2026-03-01 16:47:09 +0000

    In the useOutsideClick custom hook, I added the closeHandler parameter, which will be received and used for closing the Modal Window. Also, I wanted the user to have flexibility in deciding whether he/she wants to listen for the eventListener on capturing or on bubbling so I added the listenOnCapturing parameter with a default value of true.

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,17 +1,18 @@
 import { useEffect, useRef } from "react";
 
-export function useOutsideClick() {
+export function useOutsideClick(closeHandler, listenOnCapturing = true) {
   const clickRef = useRef();
   useEffect(
     function () {
       function handleClick(e) {
         if (clickRef.current && !clickRef.current.contains(e.target)) {
-          close();
+          closeHandler();
         }
       }
-      document.addEventListener("click", handleClick, true);
-      return () => document.removeEventListener("click", handleClick, true);
+      document.addEventListener("click", handleClick, listenOnCapturing);
+      return () =>
+        document.removeEventListener("click", handleClick, listenOnCapturing);
     },
-    [close],
+    [closeHandler, listenOnCapturing],
   );
 }
