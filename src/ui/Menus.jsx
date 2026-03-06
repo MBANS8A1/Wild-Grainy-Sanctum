84667654e6a83465db5f725d4b679dfdd35a6c7e
MBANS8A1 <seanmbng@aol.com>
2026-03-06 09:43:27 +0000

    I used the document.body as the container to display the StyledList.

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -97,7 +97,7 @@ function List({ id, children }) {
 
   if (openId !== id) return null;
 
-  return createPortal(<StyledList>{children}</StyledList>);
+  return createPortal(<StyledList>{children}</StyledList>, document.body);
 }
 
 function Button({ children }) {
