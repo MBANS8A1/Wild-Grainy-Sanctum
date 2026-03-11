914178a91697242fad0dc842347aad3b9fd5ed4b
MBANS8A1 <seanmbng@aol.com>
2026-03-11 19:39:15 +0000

    In the SortBy component, I used the useSearchParams hook  as I need to set the URL state to the value chosen from the Select component instance option value. I then used the set() function on searchParams to set the sortBy key to e.target.value (the value received by the click event).

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,7 +1,11 @@
+import { useSearchParams } from "react-router-dom";
 import Select from "./Select";
 
 function SortBy({ options }) {
-  function handleChange() {}
+  const [searchParams, setSearchParams] = useSearchParams();
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+  }
   return <Select options={options} type="white" onChange={handleChange} />;
 }
 
