fc2efaf290849e7a82742ae6d085d27c2230c2d6
MBANS8A1 <seanmbng@aol.com>
2026-03-14 12:48:11 +0000

    I plan to pass an options object into getBookings, which will help with API-side filtering of the bookings data. But first, as I need to read the new data via the URL, I used the useSearchParams in the useBookings custom hook and made a filterValue variable which will get the status values from the URL using searchParams.get().

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,7 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //Begin filtering
+  const filterValue = searchParams.get("status");
+
   const {
     isPending,
     data: bookings,
