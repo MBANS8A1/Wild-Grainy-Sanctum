be5af8ab3caa4bf58ac8bb2b077a0100a945c962
MBANS8A1 <seanmbng@aol.com>
2026-03-16 12:50:53 +0000

    I have now modified the <P> paragraph styled component with the StyledPagination component. The "Showing" portion we need to start from 1 so the currentPage needs to have 1 subtracted to make expression 0 when multiplying it by the PAGE_SIZE; 1 is then added back on to make the first page become "Showing 1". For the "to portion of the <P> styled component, it will consist of multiples of the PAGE_SIZE (which are the grouping of bookings per page); the resultCount will always be less than {currentPage * PAGE_SIZE}.

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -81,7 +81,8 @@ function Pagination({ resultCount }) {
   return (
     <StyledPagination>
       <P>
-        Showing <span>1</span> to <span>10</span> of <span>{resultCount}</span>{" "}
+        Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
+        <span>{currentPage * PAGE_SIZE}</span> of <span>{resultCount}</span>{" "}
         results
       </P>
 
