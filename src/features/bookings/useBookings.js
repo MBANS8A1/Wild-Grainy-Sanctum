02888a948038e37d11c3416575f7bf2f2488cdb2
MBANS8A1 <seanmbng@aol.com>
2026-03-17 18:50:58 +0000

    To begin the prefetching, I created the queryClient in useBookings.js using the useQueryClient hook; I then called the prefetchQuery() function on the queryClient with the same properties on the useQuery hook.

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
-
+  const queryClient = useQueryClient();
   //Begin filtering
   const filterValue = searchParams.get("status");
   const filter =
@@ -19,11 +19,20 @@ export function useBookings() {
 
   //Pagination-related
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  //Query
   const {
     isPending,
     data: { data: bookings, count } = {},
     error,
   } = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
+  });
+
+  //Pre-fetching
+
+  queryClient.prefetchQuery({
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
