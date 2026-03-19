59a9f762a1b94a1a2d97f1fb7970e7073556ad3b
MBANS8A1 <seanmbng@aol.com>
2026-03-19 12:38:13 +0000

    I added the second Menus.Button for checking in a booking that will navigate the user to the "checkin" page when clicked.

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -7,7 +7,7 @@ import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import Menus from "../../ui/Menus";
-import { HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
@@ -92,6 +92,12 @@ function BookingRow({
             onClick={() => navigate(`booking/${bookingId}`)}
           >
             See details
+          </Menus.Button>
+          <Menus.Button
+            icon={<HiArrowDownOnSquare />}
+            onClick={() => navigate(`checkin/${bookingId}`)}
+          >
+            Check in
           </Menus.Button>
         </Menus.List>
       </Menus.Menu>
