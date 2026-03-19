4fe3ee7801d960588cb0f2243e348dcc77dade2a
MBANS8A1 <seanmbng@aol.com>
2026-03-19 09:17:11 +0000

    In the useBooking custom hook, I extracted the bookingId from the URL "bookings/booking/:bookingId" using the useParams hook. I then supplied it to the getBooking function in the callback function of the queryFn on useQuery.

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+  const {
+    isPending,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBooking(bookingId),
+  });
+
+  return { isPending, cabins, error };
+}
