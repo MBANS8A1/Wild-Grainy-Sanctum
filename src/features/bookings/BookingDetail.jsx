df335a4cc158650f25f028c9bc3de7a6a3bdc8fd
MBANS8A1 <seanmbng@aol.com>
2026-03-19 13:17:39 +0000

    Within the ButtonGroup in BookingDetail.jsx I added similar logic as I did for the second Menus.Button child component in BookingRow.jsx. However, this will be an ordinary Button component.

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -11,6 +11,8 @@ import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
+import { HiArrowDownOnSquare } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -20,9 +22,8 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { isPending, booking, error } = useBooking();
-
   const moveBack = useMoveBack();
-
+  const navigate = useNavigate();
   if (isPending) return <Spinner />;
 
   const { status, id: bookingId } = booking;
@@ -46,6 +47,11 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
+        {status === "unconfirmed" && (
+          <Button onClick={() => navigate(`checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
