a5ba5c17862016ed5089f667405d9d237bc9e28a
MBANS8A1 <seanmbng@aol.com>
2026-03-20 19:37:04 +0000

    The onSuccess callback in the useMutation options object gets access to the data returned from the mutationFn (updateBooking); I have used it with a toast success message so the employee is notified that the guest/booking has been successfully checked in.

--- src/features/check-in-out/useCheckin.js
+++ src/features/check-in-out/useCheckin.js
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
 
 export function useCheckin() {
   const { mutate: checkin, isPending: isCheckingIn } = useMutation({
@@ -8,5 +9,7 @@ export function useCheckin() {
         status: "checked-in",
         isPaid: true,
       }),
+    onSuccess: (data) =>
+      toast.success(`Booking #${data.id} successfully checked in`),
   });
 }
