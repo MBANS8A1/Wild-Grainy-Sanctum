e5b1d75e33a4360495d613e7d831497daa3b6adc
MBANS8A1 <seanmbng@aol.com>
2026-03-24 12:06:21 +0000

    To ensure the Spinner component is centred while the user details are being loaded in,, I wrapped it within a FullPage (div) styled component.

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,7 +1,24 @@
 import { useUser } from "../features/authentication/useUser";
+import Spinner from "../../ui/Spinner";
+import styled from "styled-components";
 
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 function ProtectedRoute({ children }) {
   const { isPending, user } = useUser();
+
+  if (isPending)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
   return children;
 }
 
